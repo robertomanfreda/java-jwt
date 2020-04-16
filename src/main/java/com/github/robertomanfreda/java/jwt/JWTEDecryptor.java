8a129e41d273baebf4d701915c0b408c32a8df80
Roberto Manfreda <robertomanfreda1994@gmail.com>
2020-04-16 15:28:36 +0200

    [UPDATE] - Use keys to signature, encryotion, decrypption, verification; [UPDATE] - README.md

--- src/main/java/com/github/robertomanfreda/java/jwt/JWTEDecryptor.java
+++ src/main/java/com/github/robertomanfreda/java/jwt/JWTEDecryptor.java
@@ -6,17 +6,17 @@ import com.nimbusds.jose.crypto.RSADecrypter;
 import com.nimbusds.jwt.EncryptedJWT;
 import lombok.RequiredArgsConstructor;
 
-import java.security.interfaces.RSAPrivateKey;
+import java.security.PrivateKey;
 import java.text.ParseException;
 
 @RequiredArgsConstructor
 class JWTEDecryptor {
 
-    private final RSAPrivateKey rsaPrivateKey;
+    private final PrivateKey privateKey;
 
     JWEObject decrypt(String jwtString) throws ParseException, JOSEException {
         EncryptedJWT jwt = EncryptedJWT.parse(jwtString);
-        RSADecrypter decryptor = new RSADecrypter(rsaPrivateKey);
+        RSADecrypter decryptor = new RSADecrypter(privateKey);
         jwt.decrypt(decryptor);
         return jwt;
     }
--- src/main/java/com/github/robertomanfreda/java/jwt/JWTSGenerator.java
+++ src/main/java/com/github/robertomanfreda/java/jwt/JWTSGenerator.java
@@ -3,25 +3,24 @@ package com.github.robertomanfreda.java.jwt;
 import com.nimbusds.jose.JOSEObjectType;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.security.PrivateKey;
 import java.util.Map;
 
 @RequiredArgsConstructor
 class JWTSGenerator {
 
-    @Getter
-    private final String secretKey;
+    private final PrivateKey privateKey;
 
     SignedJWT generate(String issuer, String audience, Map<String, Object> claims, long ttlSeconds) throws Exception {
-        JWSHeader jwsHeader = new JWSHeader.Builder(JWSAlgorithm.HS256).type(JOSEObjectType.JWT).build();
+        JWSHeader jwsHeader = new JWSHeader.Builder(JWSAlgorithm.RS512).type(JOSEObjectType.JWT).build();
         JWTClaimsSet jwtClaimsSet = SigningUtils.generateClaimsSet(audience, claims, ttlSeconds, issuer);
         SignedJWT signedJWT = new SignedJWT(jwsHeader, jwtClaimsSet);
-        signedJWT.sign(new MACSigner(secretKey.getBytes()));
+        signedJWT.sign(new RSASSASigner(privateKey));
 
         return signedJWT;
     }
