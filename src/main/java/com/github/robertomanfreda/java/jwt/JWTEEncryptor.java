a327fb783a66be2f8ce31c7087007f1d71b6b731
Roberto Manfreda <robertomanfreda1994@gmail.com>
2020-04-16 12:11:06 +0200

    [UPDATE] - Renamed GroupId project for Maven hosting

--- src/main/java/com/github/robertomanfreda/java/jwt/JWTEEncryptor.java
+++ src/main/java/com/github/robertomanfreda/java/jwt/JWTEEncryptor.java
@@ -1,4 +1,4 @@
-package com.robertoman.java.jwt;
+package com.github.robertomanfreda.java.jwt;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.RSAEncrypter;
--- src/main/java/com/github/robertomanfreda/java/jwt/SigningUtils.java
+++ src/main/java/com/github/robertomanfreda/java/jwt/SigningUtils.java
@@ -1,4 +1,4 @@
-package com.robertoman.java.jwt;
+package com.github.robertomanfreda.java.jwt;
 
 import com.nimbusds.jwt.JWTClaimsSet;
 
