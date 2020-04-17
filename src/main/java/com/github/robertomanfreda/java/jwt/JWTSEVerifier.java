c50259cdda143cd54c97eab16bf34460560588d7
Roberto Manfreda <robertomanfreda1994@gmail.com>
2020-04-17 13:17:28 +0200

    [ADD] - Added logback; [UPDATE] - Version bump

--- src/main/java/com/github/robertomanfreda/java/jwt/JWTSEVerifier.java
+++ src/main/java/com/github/robertomanfreda/java/jwt/JWTSEVerifier.java
@@ -2,44 +2,44 @@ package com.github.robertomanfreda.java.jwt;
 
 import com.nimbusds.jose.JWEObject;
 import com.nimbusds.jose.Payload;
+import com.nimbusds.jose.crypto.RSADecrypter;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
 import com.nimbusds.jwt.SignedJWT;
 
-import java.security.PrivateKey;
-import java.security.interfaces.RSAPublicKey;
+class JWTSEVerifier2 implements IJWTSEVerifier2 {
 
-class JWTSEVerifier {
-
-    private final RSAPublicKey rsaPublicKey;
-    private final JWTEDecryptor jwTeDecryptor;
+    private final JWTEDecrypter jwTeDecrypter;
     private final JWTSVerifier jwTsVerifier;
 
-    JWTSEVerifier(RSAPublicKey rsaPublicKey, PrivateKey privateKey) {
-        this.rsaPublicKey = rsaPublicKey;
-        jwTeDecryptor = new JWTEDecryptor(privateKey);
-        jwTsVerifier = new JWTSVerifier();
+    JWTSEVerifier2(RSASSAVerifier rsassaVerifier, RSADecrypter rsaDecrypter) {
+        jwTeDecrypter = new JWTEDecrypter(rsaDecrypter);
+        jwTsVerifier = new JWTSVerifier(rsassaVerifier);
+    }
+
+    SignedJWT decryptToJWTS(String encryptedSignedJWT) throws Exception {
+        JWEObject jweObjectEncrypted = JWEObject.parse(encryptedSignedJWT);
+        JWEObject jweObjectDecrypted = jwTeDecrypter.decrypt(jweObjectEncrypted.serialize());
+        return jweObjectDecrypted.getPayload().toSignedJWT();
+    }
+
+    boolean verifyJWTs(String encryptedSignedJWT) throws Exception {
+        SignedJWT signedJWT = decryptToJWTS(encryptedSignedJWT);
+        String serialized = signedJWT.serialize();
+        return jwTsVerifier.verify(serialized);
+    }
+
+    Payload decrypt(SignedJWT signedJWT) {
+        return signedJWT.getPayload();
     }
 
     Payload verifyAndDecrypt(String encryptedSignedJWT) throws Exception {
-        boolean verified;
-        SignedJWT signedJWT;
-
-        try {
-            JWEObject jweObjectEncrypted = JWEObject.parse(encryptedSignedJWT);
-            JWEObject jweObjectDecrypted = jwTeDecryptor.decrypt(jweObjectEncrypted.serialize());
-
-            signedJWT = jweObjectDecrypted.getPayload().toSignedJWT();
-
-            String serialized = signedJWT.serialize();
-
-            verified = jwTsVerifier.verify(serialized, rsaPublicKey);
-        } catch (Exception e) {
-            throw new InvalidTokenException("Error - " + e.getMessage());
-        }
+        SignedJWT signedJWT = decryptToJWTS(encryptedSignedJWT);
+        boolean verified = verifyJWTs(encryptedSignedJWT);
 
         if (verified) {
-            return signedJWT.getPayload();
+            return decrypt(signedJWT);
         }
 
-        throw new InvalidTokenException("Invalid token or secret.");
+        throw new InvalidTokenException("Invalid.");
     }
 }
